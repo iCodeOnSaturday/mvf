b8836e5116051edf88c966535a6cdc379fd33eb6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-17 13:01:04 +0000

    Deploy iCodeOnSaturday/mvf to github.com/iCodeOnSaturday/mvf.git:gh-pages

---
+++ precache-manifest.bc63772dbcaff7e949992f3b953d66d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd503b419de5ea183923a989800b290",
+    "url": "/mvf/index.html"
+  },
+  {
+    "revision": "dade99d1c03b6e3631e4",
+    "url": "/mvf/static/js/2.daf7c7a2.chunk.js"
+  },
+  {
+    "revision": "35962e811ed21491a400",
+    "url": "/mvf/static/js/main.10bfebee.chunk.js"
+  },
+  {
+    "revision": "faa9230a625097fb1243",
+    "url": "/mvf/static/js/runtime-main.1a6b4c40.js"
+  }
+]);
